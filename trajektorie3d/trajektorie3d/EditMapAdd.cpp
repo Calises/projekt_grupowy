63887ee6fd927c907265b63a6b812811f36c1403
Sebastian M <seba_mal@op.pl>
2017-02-07 20:46:12 +0100

    poprawa kodu

--- trajektorie3d/trajektorie3d/EditMapAdd.cpp
+++ trajektorie3d/trajektorie3d/EditMapAdd.cpp
@@ -47,9 +47,9 @@ void EditMapAdd::setEditMap(std::string name, Map *m, CellState cS) {
 
 
 void EditMapAdd::accept2() {
-	QString w = ui->xLineEdit->text();
-	QString d = ui->yLineEdit->text();
-	QString h = ui->zLineEdit->text();
+	int w = (ui->xLineEdit->text()).toInt();
+	int d = (ui->yLineEdit->text()).toInt();
+	int h = (ui->zLineEdit->text()).toInt();
 
 	QPalette palette;
 	palette.setColor(QPalette::Base, Qt::white);
@@ -59,36 +59,36 @@ void EditMapAdd::accept2() {
 	ui->zLineEdit->setPalette(palette);
 	ui->errorLabel->setText("");
 	
-	if (!(w.toInt() < 1 || w.toInt() > map->getWidth() || d.toInt() < 1 || d.toInt() > map->getDepth() || h.toInt() < 1 || h.toInt() > map->getHeight())) {
+	if (!(w < 1 || w > map->getWidth() || d < 1 || d > map->getDepth() || h < 1 || h > map->getHeight())) {
 		if (cellState == CellState::Start) {
 			Cell lastCell = map->getStart();
 			Cell stopCell = map->getStop();
-			if (w.toInt() == stopCell.cell_x() && d.toInt() == stopCell.cell_y() && h.toInt() == stopCell.cell_z()) {
+			if (w == stopCell.cell_x() && d == stopCell.cell_y() && h == stopCell.cell_z()) {
 				ui->errorLabel->setText("Tutaj znajduje się STOP");
-			} else if (!(w.toInt() == lastCell.cell_x() && d.toInt() == lastCell.cell_y() && h.toInt() == lastCell.cell_z())) {
-				map->setStart(w.toInt(), d.toInt(), h.toInt());
+			} else if (!(w == lastCell.cell_x() && d == lastCell.cell_y() && h == lastCell.cell_z())) {
+				map->setStart(w, d, h);
 				map->setEmpty(lastCell.cell_x(), lastCell.cell_y(), lastCell.cell_z());
 				QDialog::accept(); // to close the dialog and return 1
 			}
 		} else if (cellState == CellState::Koniec) {
 			Cell lastCell = map->getStop();
 			Cell startCell = map->getStart();
-			if (w.toInt() == startCell.cell_x() && d.toInt() == startCell.cell_y() && h.toInt() == startCell.cell_z()) {
+			if (w == startCell.cell_x() && d == startCell.cell_y() && h == startCell.cell_z()) {
 				ui->errorLabel->setText("Tutaj znajduje się START");
-			} else if (!(w.toInt() == lastCell.cell_x() && d.toInt() == lastCell.cell_y() && h.toInt() == lastCell.cell_z())) {
-				map->setStop(w.toInt(), d.toInt());
+			} else if (!(w == lastCell.cell_x() && d == lastCell.cell_y() && h == lastCell.cell_z())) {
+				map->setStop(w, d);
 				map->setEmpty(lastCell.cell_x(), lastCell.cell_y(), lastCell.cell_z());
 				QDialog::accept(); // to close the dialog and return 1
 			}
 		} else if (cellState == CellState::Zajeta) {
 			Cell stopCell = map->getStop();
 			Cell startCell = map->getStart();
-			if (w.toInt() == startCell.cell_x() && d.toInt() == startCell.cell_y() && h.toInt() == startCell.cell_z()) {
+			if (w == startCell.cell_x() && d == startCell.cell_y() && h == startCell.cell_z()) {
 				ui->errorLabel->setText("Tutaj znajduje się START");
-			} else if (w.toInt() == stopCell.cell_x() && d.toInt() == stopCell.cell_y() && h.toInt() == stopCell.cell_z()) {
+			} else if (w == stopCell.cell_x() && d == stopCell.cell_y() && h == stopCell.cell_z()) {
 				ui->errorLabel->setText("Tutaj znajduje się STOP");
 			} else {
-				map->setObstacle(w.toInt(), d.toInt(), h.toInt());
+				map->setObstacle(w, d, h);
 				QDialog::accept(); // to close the dialog and return 1
 			}
 		}
@@ -96,13 +96,13 @@ void EditMapAdd::accept2() {
 	else {
 		palette.setColor(QPalette::Base, Qt::red);
 		palette.setColor(QPalette::Text, Qt::black);
-		if (w.toInt() < 1 || w.toInt() > map->getWidth()) {
+		if (w < 1 || w > map->getWidth()) {
 			ui->xLineEdit->setPalette(palette);
 		}
-		if (d.toInt() < 1 || d.toInt() > map->getDepth()) {
+		if (d < 1 || d > map->getDepth()) {
 			ui->yLineEdit->setPalette(palette);
 		}
-		if (h.toInt() < 1 || h.toInt() > map->getHeight()) {
+		if (h < 1 || h > map->getHeight()) {
 			ui->zLineEdit->setPalette(palette);
 		}
 		ui->errorLabel->setText("Nieprawidłowy zakres");
